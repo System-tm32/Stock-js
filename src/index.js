a16edd14f2d96046560e8bc53887b9d1a563a3d3
Kirill Ilin <Systemtm32@yandex.ru>
2020-06-02 16:55:16 +0300

    add: view components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './scss/index.scss'
+import {Excel} from './components/excel/Excel'
+import {Header} from './components/header/Header'
+import {ToolBar} from './components/toolbar/ToolBar'
+import {Formula} from './components/formula/formula'
+import {Table} from './components/table/Table'
 
+const excel = new Excel('#app', {
+	components: [Header, ToolBar, Formula, Table]
+})
+excel.render()
